9dc2a01d0eb1ff2b9a288909d1289043c2ae15fb
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-07-02 18:15:54 +0200

    Update api endpoint example

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { ApiResponse } from "@danielmat/api-utils"
+import type { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
   name: string
@@ -7,7 +8,10 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ApiResponse<Data>>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    status: "success",
+    name: "John Doe",
+  })
 }
